b33e32b61a1edf516c8bdc230253490529d4aa43
Chrischuka13 <divineprovidence30@gmail.com>
2026-04-05 19:29:51 +0100

    cors config

--- index.js
+++ index.js
@@ -7,19 +7,11 @@ const app = express()
 const PORT = process.env.PORT || 9000
 import Subscribe from "./routes/subsribe.js"
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://rebornclassics.vercel.app"
-];
-
 app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
-}));
-
-app.options("*", cors({
-  origin: allowedOrigins,
-  credentials: true
+  origin: ['http://localhost:5173', 'https://rebornclassics.vercel.app'],
+  credentials: true,
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
